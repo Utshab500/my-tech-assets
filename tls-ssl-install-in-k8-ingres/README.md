b126306e5049b3f130a6d8f88419f0555ef87647
Utshab Saha <utshab.saha@accenture.com>
2025-03-06 21:59:11 +0800

    adding tls/ssl installation in kubernetes ingress poc

---
+++ tls-ssl-install-in-k8-ingres/README.md
@@ -0,0 +1,30 @@
+# Description
+Here in this POC I install TLS/SSL certificate in **kubernetes ingress**
+
+## Steps
+### Used Domain name in the POC
+`myapp.utshab.com`
+
+### Create a dev namespace.
+```bash
+kubectl create ns dev
+```
+
+### Deploy application
+```bash
+kubectl apply -f hello-app.yaml 
+```
+
+### Create a Kubernetes TLS Secret
+Run the below command from the directory where `<server>.crt` is present. In my case `openssl` since I have used `generate-ssl.sh` script to create the `self-signed` certficate and it creates the `openssl` folder.
+- If want to redo the TLS/SSL cert generation, just remove `openssl` folder and execute `generate-ssl.sh`
+```bash
+kubectl create secret tls hello-app-tls \
+    --namespace dev \
+    --key <server>.key \
+    --cert <server>.crt
+```
+
+## Reference
+- [How To Configure Ingress TLS/SSL Certificates in Kubernetes](https://devopscube.com/configure-ingress-tls-kubernetes/)
+- [How to Create Self-Signed Certificates using OpenSSL](https://devopscube.com/create-self-signed-certificates-openssl/)
---
+++ tls-ssl-install-in-k8-ingres/generate-ssl.sh
@@ -0,0 +1,79 @@
+#! /bin/bash
+
+# Used Domain name - myapp.utshab.com
+
+set -e
+
+if [ "$#" -ne 1 ]
+then
+  echo "Error: No domain name argument provided"
+  echo "Usage: Provide a domain name as an argument"
+  exit 1
+fi
+
+DOMAIN=$1
+
+mkdir openssl && cd openssl
+
+# Create selfsigned root CA certfifcate (rootCA.key and rootCA.crt)
+openssl req -x509 \
+            -sha256 -days 356 \
+            -nodes \
+            -newkey rsa:2048 \
+            -subj "/CN=${DOMAIN}/C=SG/L=Yishun" \
+            -keyout rootCA.key -out rootCA.crt 
+
+# Create the sercer private key
+openssl genrsa -out ${DOMAIN}.key 2048
+
+# csr.conf that holds the information to generate CSR
+cat > csr.conf <<EOF
+[ req ]
+default_bits = 2048
+prompt = no
+default_md = sha256
+req_extensions = req_ext
+distinguished_name = dn
+
+[ dn ]
+C = SG
+ST = Yishun
+L = Yishun
+O = MyApp
+OU = MyApp Dev
+CN = ${DOMAIN}
+
+[ req_ext ]
+subjectAltName = @alt_names
+
+[ alt_names ]
+DNS.1 = ${DOMAIN}
+DNS.2 = www.${DOMAIN}
+IP.1 = 192.168.1.5
+IP.2 = 192.168.1.6
+
+EOF
+
+# Generate Certificate Signing Request (CSR) Using Server Private Key
+openssl req -new -key ${DOMAIN}.key -out ${DOMAIN}.csr -config csr.conf
+
+# Create cert.conf
+cat > cert.conf <<EOF
+
+authorityKeyIdentifier=keyid,issuer
+basicConstraints=CA:FALSE
+keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
+subjectAltName = @alt_names
+
+[alt_names]
+DNS.1 = ${DOMAIN}
+
+EOF
+
+# Generate SSL certificate With self signed CA
+openssl x509 -req \
+    -in ${DOMAIN}.csr \
+    -CA rootCA.crt -CAkey rootCA.key \
+    -CAcreateserial -out ${DOMAIN}.crt \
+    -days 365 \
+    -sha256 -extfile cert.conf
