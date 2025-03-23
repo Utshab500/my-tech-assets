d7418712449a01b80ebb1d6bb8801290475a05fd
Utshab Saha <utshab500@gmail.com>
2025-03-23 15:27:35 +0800

    readiness-probe poc

--- k8-single-ingress-to-multiple-backend/node-project/index.js
+++ k8-single-ingress-to-multiple-backend/node-project/index.js
@@ -12,36 +12,25 @@ app.use(express.json());
 const users = [];
 
 // GET endpoint to fetch all users
-app.get("/api/users", (req, res) => {
+app.get("/", (req, res) => {
+  res.json({
+    message: "OK"
+  });
+});
+
+// GET endpoint to fetch all users
+app.get("/node/health", (req, res) => {
+  res.json({
+    message: "health OK"
+  });
+});
+
+// GET endpoint to fetch all users
+app.get("/node/users", (req, res) => {
   res.json({
     message: "users fetched successfully",
     data: users,
   });
-});
-
-// POST endpoint to create a new user
-app.post("/api/users", (req, res) => {
-  const { username, password } = req.body;
-
-  if (!username || !password) {
-    return res
-      .status(400)
-      .json({ error: "username and password are required" });
-  }
-
-  // Check if username already exists
-  if (users.some((user) => user.username === username)) {
-    return res.status(400).json({ error: "Username already exists" });
-  }
-
-  const newUser = {
-    id: users.length + 1,
-    username,
-    password,
-  };
-
-  users.push(newUser);
-  res.status(201).json(newUser);
 });
 
 app.listen(PORT, () => {
