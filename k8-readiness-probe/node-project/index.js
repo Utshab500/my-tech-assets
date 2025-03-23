173e1924522448c78f5b20335850d7fbf77f59b3
Utshab Saha <utshab500@gmail.com>
2025-03-23 15:30:24 +0800

    readiness-probe poc

---
+++ k8-readiness-probe/node-project/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// In-memory storage
+const users = [];
+
+// GET endpoint to fetch all users
+app.get("/", (req, res) => {
+  res.json({
+    message: "OK"
+  });
+});
+
+// GET endpoint to fetch all users
+app.get("/node/health", (req, res) => {
+  res.json({
+    message: "health OK"
+  });
+});
+
+// GET endpoint to fetch all users
+app.get("/node/users", (req, res) => {
+  res.json({
+    message: "users fetched successfully",
+    data: users,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
