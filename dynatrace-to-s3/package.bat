5798e99065467f81f0a0cee723407840b2fa3864
Utshab Saha <utshab.saha@accenture.com>
2026-09-16 16:58:53 +0800

    dynatrace to s3 function

---
+++ dynatrace-to-s3/package.bat
@@ -0,0 +1,19 @@
+@echo off
+
+set ZIP_FILE=lambda_package.zip
+set PACKAGE_DIR=package
+
+if exist %ZIP_FILE% del %ZIP_FILE%
+if exist %PACKAGE_DIR% rmdir /s /q %PACKAGE_DIR%
+
+mkdir %PACKAGE_DIR%
+
+pip install -r requirements.txt -t %PACKAGE_DIR% --quiet
+
+copy fetch_audit_logs.py %PACKAGE_DIR%
+
+powershell -Command "Compress-Archive -Path '%PACKAGE_DIR%/*' -DestinationPath '%ZIP_FILE%'"
+
+rmdir /s /q %PACKAGE_DIR%
+
+echo Created %ZIP_FILE%
